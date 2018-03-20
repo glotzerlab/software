16884a640eac9d139f0b8fee38f4283420eec4c8
Joshua A. Anderson <joaander@umich.edu>
2018-03-20 09:28:38 -0400

    Build with gcc 4.9 on titan

--- make_dockerfiles.py
+++ make_dockerfiles.py
@@ -33,6 +33,8 @@ software_template = env.get_template('software.jinja')
 write('cuda8/Dockerfile', [base_template, software_template],
       FROM='nvidia/cuda:8.0-devel-ubuntu16.04',
       ENABLE_MPI='off',
+      CC='/usr/bin/gcc',
+      CXX='/usr/bin/g++',
       **versions)
 
 write('cuda8/openmpi3.0/Dockerfile', [base_template, openmpi_template, software_template],
@@ -40,9 +42,13 @@ write('cuda8/openmpi3.0/Dockerfile', [base_template, openmpi_template, software_
       OPENMPI_VERSION='3.0.0',
       OSU_MICROBENCHMARK_VERSION='5.4.1',
       ENABLE_MPI='on',
+      CC='/usr/bin/gcc',
+      CXX='/usr/bin/g++',
       **versions)
 
 write('olcf-titan/Dockerfile', [base_template, software_template],
       FROM='olcf/titan:ubuntu-16.04_2018-01-18',
       ENABLE_MPI='on',
+      CC='/usr/bin/gcc-4.9',
+      CXX='/usr/bin/g++-4.9',
       **versions)
