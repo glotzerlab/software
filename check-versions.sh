9b75e25d60cce407a68d4bd8fc2bbdcccbe7eec2
Joshua A. Anderson <joaander@umich.edu>
2018-04-04 10:49:00 -0400

    Add version checking script

---
+++ check-versions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+packages="fresnel freud gsd hoomd-blue signac signac-flow"
+
+output="package git-version pkgver needs-update\n"
+for package in ${packages}
+do
+    repos=git+ssh://bitbucket.org/glotzer/${package}
+
+    git_version=$(git ls-remote --tags ${repos} | sort -t '/' -k 3 -k 4 -V | grep -v { | awk -F/ '{ print $3 }' | tail -n1)
+    if [ -z "${git_version}" ]
+    then
+        git_version="n/a"
+    fi
+
+    pkgver=$( python -c "import make_dockerfiles; print(make_dockerfiles.repo_version['$package']);" )
+    prefix=""
+
+    if [[ "${git_version}" == "${prefix}${pkgver}" ]]
+    then
+        update="no"
+    else
+        update="yes"
+    fi
+
+    output+="${package} ${git_version} ${pkgver} ${update}\n"
+done
+
+echo -e ${output} | column -t
--- make_dockerfiles.py
+++ make_dockerfiles.py
@@ -7,46 +7,48 @@ base_repository = 'glotzerlab/base'
 
 # software versions
 versions = {}
+repo_version = {}
 
 # dependencies
 versions['EMBREE_VERSION'] = '2.17.1'
 
 # glotzer lab
-versions['FRESNEL_VERSION'] = 'v0.5.0'
-versions['FREUD_VERSION'] = 'v0.7.0'
-versions['GSD_VERSION'] = 'v1.5.1'
-versions['HOOMD_VERSION'] = 'v2.2.4'
-versions['SIGNAC_VERSION'] = 'v0.9.2'
-versions['SIGNAC_FLOW_VERSION'] = 'v0.5.6'
+repo_version['fresnel']     = versions['FRESNEL_VERSION']     = 'v0.5.0'
+repo_version['freud']       = versions['FREUD_VERSION']       = 'v0.7.0'
+repo_version['gsd']         = versions['GSD_VERSION']         = 'v1.5.2'
+repo_version['hoomd-blue']  = versions['HOOMD_VERSION']       = 'v2.2.4'
+repo_version['signac']      = versions['SIGNAC_VERSION']      = 'v0.9.2'
+repo_version['signac-flow'] = versions['SIGNAC_FLOW_VERSION'] = 'v0.5.6'
 
+if __name__ == '__main__':
 
-def write(fname, templates, **kwargs):
-    with open(fname, 'w') as f:
-        for template in templates:
-            f.write(template.render(**kwargs));
+    def write(fname, templates, **kwargs):
+        with open(fname, 'w') as f:
+            for template in templates:
+                f.write(template.render(**kwargs));
 
-env = jinja2.Environment(loader=jinja2.FileSystemLoader('template'))
-base_template = env.get_template('base.jinja')
-openmpi_template = env.get_template('openmpi.jinja')
-titan_template = env.get_template('titan.jinja')
-software_template = env.get_template('software.jinja')
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader('template'))
+    base_template = env.get_template('base.jinja')
+    openmpi_template = env.get_template('openmpi.jinja')
+    titan_template = env.get_template('titan.jinja')
+    software_template = env.get_template('software.jinja')
 
-write('cuda8/Dockerfile', [base_template, software_template],
-      FROM='nvidia/cuda:8.0-devel-ubuntu16.04',
-      ENABLE_MPI='off',
-      MAKEJOBS=10,
-      **versions)
+    write('cuda8/Dockerfile', [base_template, software_template],
+          FROM='nvidia/cuda:8.0-devel-ubuntu16.04',
+          ENABLE_MPI='off',
+          MAKEJOBS=10,
+          **versions)
 
-write('cuda8/openmpi3.0/Dockerfile', [base_template, openmpi_template, software_template],
-      FROM='nvidia/cuda:8.0-devel-ubuntu16.04',
-      OPENMPI_VERSION='3.0.0',
-      OSU_MICROBENCHMARK_VERSION='5.4.1',
-      ENABLE_MPI='on',
-      MAKEJOBS=10,
-      **versions)
+    write('cuda8/openmpi3.0/Dockerfile', [base_template, openmpi_template, software_template],
+          FROM='nvidia/cuda:8.0-devel-ubuntu16.04',
+          OPENMPI_VERSION='3.0.0',
+          OSU_MICROBENCHMARK_VERSION='5.4.1',
+          ENABLE_MPI='on',
+          MAKEJOBS=10,
+          **versions)
 
-write('olcf-titan/Dockerfile', [base_template, titan_template, software_template],
-      FROM='olcf/titan:ubuntu-16.04_2018-01-18',
-      ENABLE_MPI='on',
-      MAKEJOBS=2,
-      **versions)
+    write('olcf-titan/Dockerfile', [base_template, titan_template, software_template],
+          FROM='olcf/titan:ubuntu-16.04_2018-01-18',
+          ENABLE_MPI='on',
+          MAKEJOBS=2,
+          **versions)
