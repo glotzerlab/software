dc2a7a7187e9538e99d890dd2c3da439bf0c4a7c
Joshua A. Anderson <joaander@umich.edu>
2018-12-05 14:38:26 -0500

    Use cuda9 in image builds

--- build.sh
+++ build.sh
@@ -6,45 +6,45 @@ set -e
 DATE_TAG=$(date +%Y.%m)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker build $DIR/cuda8 \
+docker build $DIR/docker \
              -t glotzerlab/software:devel \
 
 cp $DIR/test/*.py $DIR/docker/nompi
 docker build $DIR/docker/nompi \
              -t glotzerlab/software:latest \
-             -t glotzerlab/software:${DATE_TAG}-cuda8
+             -t glotzerlab/software:${DATE_TAG}-cuda9
 echo "BootStrap: docker
-From: glotzerlab/software:${DATE_TAG}-cuda8
+From: glotzerlab/software:${DATE_TAG}-cuda9
 " > $DIR/docker/Singularity.latest
 
 cp $DIR/test/*.py $DIR/docker/flux
 docker build $DIR/docker/flux \
              -t glotzerlab/software:flux \
-             -t glotzerlab/software:${DATE_TAG}-cuda8-mlx-openmpi3.0.0
+             -t glotzerlab/software:${DATE_TAG}-cuda9-mlx-openmpi3.0.0
 echo "BootStrap: docker
-From: glotzerlab/software:${DATE_TAG}-cuda8-mlx-openmpi3.0.0
+From: glotzerlab/software:${DATE_TAG}-cuda9-mlx-openmpi3.0.0
 " > $DIR/docker/flux/Singularity.flux
 
 cp $DIR/test/*.py $DIR/docker/comet
 docker build $DIR/docker/comet \
              -t glotzerlab/software:comet \
-             -t glotzerlab/software:${DATE_TAG}-haswell-cuda8-mlx-openmpi1.8.4
+             -t glotzerlab/software:${DATE_TAG}-haswell-cuda9-mlx-openmpi1.8.4
 echo "BootStrap: docker
-From: glotzerlab/software:${DATE_TAG}-haswell-cuda8-mlx-openmpi1.8.4
+From: glotzerlab/software:${DATE_TAG}-haswell-cuda9-mlx-openmpi1.8.4
 " > $DIR/docker/comet/Singularity.comet
 
 cp $DIR/test/*.py $DIR/docker/bridges
 docker build $DIR/docker/bridges \
              -t glotzerlab/software:bridges \
-             -t glotzerlab/software:${DATE_TAG}-haswell-cuda8-hfi1-openmpi2.1.2
+             -t glotzerlab/software:${DATE_TAG}-haswell-cuda9-hfi1-openmpi2.1.2
 echo "BootStrap: docker
-From: glotzerlab/software:${DATE_TAG}-haswell-cuda8-hfi1-openmpi2.1.2
+From: glotzerlab/software:${DATE_TAG}-haswell-cuda9-hfi1-openmpi2.1.2
 " > $DIR/docker/bridges/Singularity.bridges
 
 cp $DIR/test/*.py $DIR/docker/stampede2
 docker build $DIR/docker/stampede2 \
              -t glotzerlab/software:stampede2 \
-             -t glotzerlab/software:${DATE_TAG}-skylakex-cuda8-hfi1-mvapich2.3
+             -t glotzerlab/software:${DATE_TAG}-skylakex-cuda9-hfi1-mvapich2.3
 echo "BootStrap: docker
-From: glotzerlab/software:${DATE_TAG}-skylakex-cuda8-hfi1-mvapich2.3
+From: glotzerlab/software:${DATE_TAG}-skylakex-cuda9-hfi1-mvapich2.3
 " > $DIR/docker/stampede2/Singularity.stampede2
