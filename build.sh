997bed628d875ab85d93e614c95d5448a43e52e3
Joshua A. Anderson <joaander@umich.edu>
2020-10-08 09:55:34 -0400

    Add beta name to build tags

--- build.sh
+++ build.sh
@@ -47,11 +47,19 @@ OUTPUT=`mktemp -d -t singularity.XXXXXXX`
 umask 022
 
 declare -A extra_tags
-extra_tags=( ["nompi"]="-t ${repository}/software:latest -t ${repository}/software:${tag}-cuda10"
-             ["greatlakes"]="-t ${repository}/software:${tag}-skylakex-cuda10-mlx-openmpi4.0.1"
-             ["comet"]="-t ${repository}/software:${tag}-haswell-cuda9-mlx-openmpi1.8.4"
-             ["bridges"]="-t ${repository}/software:${tag}-haswell-cuda10-hfi1-openmpi2.1.2"
-             ["stampede2"]="-t ${repository}/software:${tag}-skylakex-cuda10-hfi1-mvapich2.3"
+# extra_tags=( ["nompi"]="-t ${repository}/software:latest -t ${repository}/software:${tag}-cuda10"
+#              ["greatlakes"]="-t ${repository}/software:${tag}-skylakex-cuda10-mlx-openmpi4.0.1"
+#              ["comet"]="-t ${repository}/software:${tag}-haswell-cuda9-mlx-openmpi1.8.4"
+#              ["bridges"]="-t ${repository}/software:${tag}-haswell-cuda10-hfi1-openmpi2.1.2"
+#              ["stampede2"]="-t ${repository}/software:${tag}-skylakex-cuda10-hfi1-mvapich2.3"
+# )
+
+# Don't tag beta buidls with extra tags
+extra_tags=( ["nompi"]=""
+             ["greatlakes"]=""
+             ["comet"]=""
+             ["bridges"]=""
+             ["stampede2"]=""
 )
 
 if [ "$skip_docker" = false ] ; then
@@ -60,8 +68,8 @@ if [ "$skip_docker" = false ] ; then
         cp -a $DIR/test/*.py $DIR/docker/${cluster}/test
         cp -a $DIR/check-requirements.py $DIR/docker/${cluster}
         docker build $DIR/docker/${cluster} \
-                    -t ${repository}/software:${cluster} \
-                    -t ${repository}/software:${tag}-${cluster} \
+                    -t ${repository}/software:${cluster}-beta \
+                    -t ${repository}/software:${tag}-${cluster}-beta \
                     ${extra_tags[$cluster]}
     done
 fi
@@ -71,7 +79,7 @@ if [ "$build_singularity" = true ] ; then
     do
         docker run -t --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock \
                    -v ${OUTPUT}:/output singularityware/docker2singularity:v2.6 \
-                   --name software-${label} ${repository}/software:${tag}-${label}
+                   --name software-${label}-beta ${repository}/software:${tag}-${label}-beta
         mkdir -p /nfs/turbo/glotzer/containers/${repository}
         mv ${OUTPUT}/*.simg /nfs/turbo/glotzer/containers/${repository}
     done
