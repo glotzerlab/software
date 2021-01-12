96357ea3ad6e38948de0b70d69d76fee11383be9
Joshua A. Anderson <joaander@umich.edu>
2021-01-12 10:36:36 -0500

    Update check version script

--- check-versions.sh
+++ check-versions.sh
@@ -2,7 +2,7 @@
 
 packages="fresnel freud garnett gsd hoomd-blue libgetar pythia rowan plato signac signac-flow"
 
-output="package git-version pkgver needs-update\n"
+output="package git-version pkgver\n"
 for package in ${packages}
 do
     repos=https://github.com/glotzerlab/${package}
@@ -16,14 +16,7 @@ do
     pkgver=$( python -c "import make_dockerfiles; print(make_dockerfiles.repo_version['$package']);" )
     prefix=""
 
-    if [[ "${git_version}" == "${prefix}${pkgver}" ]]
-    then
-        update="no"
-    else
-        update="yes"
-    fi
-
-    output+="${package} ${git_version} ${pkgver} ${update}\n"
+    output+="${package} ${git_version} ${pkgver}\n"
 done
 
 echo -e ${output} | column -t
