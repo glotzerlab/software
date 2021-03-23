335eb31fc389a643402ba1a8b6f5d4080f2e945e
Joshua A. Anderson <joaander@umich.edu>
2021-03-23 19:40:13 -0400

    Bump software to latest versions

--- make_dockerfiles.py
+++ make_dockerfiles.py
@@ -28,19 +28,19 @@ versions['SCIPY_VERSION'] = '1.5.2'
 shas['SCIPY_SHA'] = '066c513d90eb3fd7567a9e150828d39111ebd88d3e924cdfc9f8ce19ab6f90c9'
 
 # glotzer lab
-repo_version['fresnel']     = versions['FRESNEL_VERSION']     = 'v0.12.0'
+repo_version['fresnel']     = versions['FRESNEL_VERSION']     = 'v0.13.1'
 repo_version['freud']       = versions['FREUD_VERSION']       = 'v2.4.1'
 repo_version['fsph']        = versions['FSPH_VERSION']        = 'v0.2.0'
 repo_version['garnett']     = versions['GARNETT_VERSION']     = 'v0.7.1'
-repo_version['gsd']         = versions['GSD_VERSION']         = 'v2.4.0'
-repo_version['hoomd-blue']  = versions['HOOMD_VERSION']       = 'v2.9.4'
-repo_version['libgetar']    = versions['LIBGETAR_VERSION']    = 'v1.0.1'
+repo_version['gsd']         = versions['GSD_VERSION']         = 'v2.4.1'
+repo_version['hoomd-blue']  = versions['HOOMD_VERSION']       = 'v2.9.6'
+repo_version['libgetar']    = versions['LIBGETAR_VERSION']    = 'v1.1.0'
 repo_version['pythia']      = versions['PYTHIA_VERSION']      = 'v0.2.5'
 repo_version['rowan']       = versions['ROWAN_VERSION']       = 'v1.3.0.post1'
 repo_version['coxeter']     = versions['COXETER_VERSION']     = 'v0.4.0'
 repo_version['plato']       = versions['PLATO_VERSION']       = 'v1.7.0'
 repo_version['signac']      = versions['SIGNAC_VERSION']      = 'v1.6.0'
-repo_version['signac-flow'] = versions['SIGNAC_FLOW_VERSION'] = 'v0.12.0'
+repo_version['signac-flow'] = versions['SIGNAC_FLOW_VERSION'] = 'v0.13.0'
 
 if __name__ == '__main__':
 
