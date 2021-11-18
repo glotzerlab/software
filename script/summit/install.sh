b46727371a67eaf6b0eeb3f2bf3309130cd40575
joaander <joaander@users.noreply.github.com>
2021-11-18 11:15:51 +0000

    [update-packages] Bump package versions

--- script/summit/install.sh
+++ script/summit/install.sh
@@ -146,7 +146,7 @@ fi
 
 
 
- git clone --recursive --branch v3.0.0-beta.10 --depth 1 https://github.com/glotzerlab/hoomd-blue hoomd \
+ git clone --recursive --branch v3.0.0-beta.11 --depth 1 https://github.com/glotzerlab/hoomd-blue hoomd \
     && cd hoomd \
     && mkdir -p build \
     && cd build \
