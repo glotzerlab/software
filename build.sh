fb37b550aff3031e580d7ee8f3254e7a310e5019
Joshua A. Anderson <joaander@umich.edu>
2022-03-08 08:51:35 -0500

    Remove v3 beta specific build parameters.

--- build.sh
+++ build.sh
@@ -36,9 +36,7 @@ python3 make_dockerfiles.py
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 declare -A extra_tags
-# extra_tags=( ["nompi"]="-t ${repository}/software:latest"
-# Don't tag beta builds with extra tags
-extra_tags=( ["nompi"]=""
+extra_tags=( ["nompi"]="-t ${repository}/software:latest"
              ["greatlakes"]=""
              ["expanse"]=""
              ["expanse-gpu"]=""
@@ -50,11 +48,11 @@ do
     cp -a $DIR/test/*.py $DIR/docker/${cluster}/test
     cp -a requirements*.txt $DIR/docker/${cluster}
     docker build $DIR/docker/${cluster} \
-                -t ${repository}/software:beta-${cluster} \
-                -t ${repository}/software:${tag}-beta-${cluster} \
+                -t ${repository}/software:${cluster} \
+                -t ${repository}/software:${tag}-${cluster} \
                 ${extra_tags[$cluster]} \
                 --build-arg GIT_SHA=$(git rev-parse HEAD) \
                 --build-arg GIT_BRANCH=$(git branch --show-current) \
                 --build-arg CONFIGURATION=${cluster} \
-                --build-arg TAG=${tag}-beta
+                --build-arg TAG=${tag}
 done
