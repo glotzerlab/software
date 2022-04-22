861a8fa10afa91f64d5d2053358e229bbe2d2e03
Brandon Butler <butlerbr@umich.edu>
2022-04-22 16:07:18 -0400

    Modify dependencies for pathways project

--- script/summit/install.sh
+++ script/summit/install.sh
@@ -146,7 +146,7 @@ fi
 
 
 
- git clone --recursive --branch v3.0.0-beta.14 --depth 1 https://github.com/glotzerlab/hoomd-blue hoomd \
+ git clone --recursive --branch v3.0.1 --depth 1 https://github.com/glotzerlab/hoomd-blue hoomd \
     && cd hoomd \
     && mkdir -p build \
     && cd build \
