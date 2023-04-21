54e6edb45d96e0b319b64836aca9b5b7c09aa0f3
Joshua A. Anderson <joaander@umich.edu>
2023-04-21 11:48:34 -0400

    Install cupy only on systems that support it.

--- script/crusher/install.sh
+++ script/crusher/install.sh
@@ -133,7 +133,6 @@ fi
 
 
 # Install packages that are build requirements of other packages first.
-# lapack is needed for scipy.
 # Use the pip cache in script builds to reduce time when rerunning the install script.
 
 
@@ -143,8 +142,10 @@ fi
 
 
  export CFLAGS="-march=native" CXXFLAGS="-march=native" \
-    && python3 -m pip install --no-build-isolation -r requirements.txt \
+    && python3 -m pip install --no-build-isolation --no-binary freud-analysis,gsd -r requirements.txt \
     || exit 1
+
+
 
 
 
@@ -189,8 +190,3 @@ then
 
 
 fi
-# cupy does not support the cuda array interface on crusher
-pip uninstall -y cupy
-
-# pip uninstall does not remote the cupy directory, so `import cupy` still works:
-rm -rf $ROOT/lib/python*/site-packages/cupy
