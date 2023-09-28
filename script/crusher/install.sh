b000869a5abd4e914bf72134015aac96b798fb03
joaander <joaander@users.noreply.github.com>
2023-09-28 15:09:21 +0000

    [update-packages] Bump package versions

--- script/crusher/install.sh
+++ script/crusher/install.sh
@@ -89,8 +89,8 @@ fi
 # Embree
 if [ ! -f $ROOT/lib64/libembree4.so ]
 then
-curl -sSL https://github.com/embree/embree/archive/v4.2.0/embree-4.2.0.tar.gz | tar -xzC $BUILDDIR \
-    && cd $BUILDDIR/embree-4.2.0 \
+curl -sSL https://github.com/embree/embree/archive/v4.3.0/embree-4.3.0.tar.gz | tar -xzC $BUILDDIR \
+    && cd $BUILDDIR/embree-4.3.0 \
     && mkdir build && cd build \
     && cmake ../ -DCMAKE_INSTALL_PREFIX=$ROOT -DCMAKE_INSTALL_LIBDIR=lib64/ -DCMAKE_BUILD_TYPE=Release -DEMBREE_TUTORIALS=OFF -DEMBREE_MAX_ISA="AVX2" -DEMBREE_ISPC_SUPPORT=OFF \
     && make install -j 32 \
@@ -163,7 +163,7 @@ then
     && mkdir -p build \
     && cd build \
     && export CFLAGS="-march=native" CXXFLAGS="-march=native" \
-    && cmake ../ -DENABLE_EMBREE=on -DENABLE_OPTIX=off -Dembree_DIR=/opt/embree-4.2.0.x86_64.linux -DCMAKE_INSTALL_PREFIX=`python3 -c "import site; print(site.getsitepackages()[0])"` \
+    && cmake ../ -DENABLE_EMBREE=on -DENABLE_OPTIX=off -Dembree_DIR=/opt/embree-4.3.0.x86_64.linux -DCMAKE_INSTALL_PREFIX=`python3 -c "import site; print(site.getsitepackages()[0])"` \
     && make install -j32 \
     && cd ../../ \
     && rm -rf fresnel \
