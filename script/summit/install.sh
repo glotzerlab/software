7e01b3b899f6f4cbfac3e9f731f4324edb830921
joaander <joaander@users.noreply.github.com>
2023-10-03 13:37:48 +0000

    [update-packages] Bump package versions

--- script/summit/install.sh
+++ script/summit/install.sh
@@ -171,7 +171,7 @@ done <requirements-source-summit.txt
 if [ ! -n "$(ls -d $ROOT/lib/python*/site-packages/hoomd)" ]
 then
 
- git clone --recursive --branch v4.2.0 --depth 1 https://github.com/glotzerlab/hoomd-blue hoomd \
+ git clone --recursive --branch v4.2.1 --depth 1 https://github.com/glotzerlab/hoomd-blue hoomd \
     && cd hoomd \
     && mkdir -p build \
     && cd build \
