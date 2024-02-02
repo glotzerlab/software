955e8de1711efadb600bec416d01f27e985dc578
Joshua A. Anderson <joaander@umich.edu>
2024-02-02 16:22:28 -0500

    Support NCSA delta slingshot11 hardware update.

--- docker/delta/test/job-gpu.sh
+++ docker/delta/test/job-gpu.sh
@@ -7,18 +7,16 @@
 #SBATCH --export=ALL
 #SBATCH -t 0:10:00
 
-module load gcc/11.2.0 openmpi/4.1.4
+module load gcc/11.4.0 openmpi/4.1.6
 
 set -x
 
-export OMPI_MCA_btl=self
-
 singularity exec software.sif bash -c "set" | grep GLOTZERLAB
 
-srun -n 1 singularity exec --nv software.sif python3 serial-gpu.py
+mpirun -n 1 singularity exec --nv software.sif python3 serial-gpu.py
 
-srun singularity exec --nv software.sif python3 mpi-gpu.py
+mpirun singularity exec --nv software.sif python3 mpi-gpu.py
 
-srun singularity exec software.sif /opt/osu-micro-benchmarks/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_bibw
+mpirun singularity exec software.sif /opt/osu-micro-benchmarks/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_bibw
 
 echo "Tests complete."
