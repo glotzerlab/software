a30d855860b78f1d4403fe4739b1d08e4b5076a3
Joshua A. Anderson <joaander@umich.edu>
2024-07-18 09:58:40 -0400

    Fix build on Frontier.

--- conda/hoomd/build_hoomd.sh
+++ conda/hoomd/build_hoomd.sh
@@ -5,6 +5,7 @@ fi
 # Use CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE to keep absolute links to mpi and other modules
 cmake -B build \
       ${CMAKE_ARGS} \
+      -DBUILD_MPCD=off \
       -DENABLE_MPI=on \
       -DENABLE_GPU=${ENABLE_GPU:-off} \
       -DHOOMD_GPU_PLATFORM=$2 \
