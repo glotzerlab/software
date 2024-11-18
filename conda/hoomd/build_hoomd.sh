1716e3457eb68179108a5e79e5e307d72c0ec309
Joshua A. Anderson <joaander@umich.edu>
2024-11-18 11:31:21 -0500

    Build 4.9.1

--- conda/hoomd/build_hoomd.sh
+++ conda/hoomd/build_hoomd.sh
@@ -5,7 +5,6 @@ fi
 # Use CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE to keep absolute links to mpi and other modules
 cmake -B build \
       ${CMAKE_ARGS} \
-      -DBUILD_MPCD=on \
       -DENABLE_MPI=on \
       -DENABLE_GPU=${ENABLE_GPU:-off} \
       -DHOOMD_GPU_PLATFORM=$2 \
