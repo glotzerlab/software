62194f92d2ee01ed4d0768a36c548bbaa94856e9
Joshua A. Anderson <joaander@umich.edu>
2024-12-03 09:18:10 -0500

    Bump to 5.0

--- conda/hoomd/build_hoomd.sh
+++ conda/hoomd/build_hoomd.sh
@@ -9,8 +9,6 @@ cmake -B build \
       -DENABLE_GPU=${ENABLE_GPU:-off} \
       -DHOOMD_GPU_PLATFORM=$2 \
       -DBUILD_TESTING=off \
-      -DENABLE_TBB=off \
-      -DENABLE_LLVM=off \
       -DPLUGINS="" \
       -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE \
       -DCMAKE_CXX_FLAGS=-march=native -DCMAKE_C_FLAGS=-march=native \
